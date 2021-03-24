1232179bc4c89919b39fee4a1a768d65e9d71e69
Accurics <admin@accurics.com>
2021-03-24 10:57:51 -0500

    Auto Remediation By Accurics

--- aws/security_group.tf
+++ aws/security_group.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "aws-demo-security-group" {
     to_port     = 22
     from_port   = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.1.1.1/32"]
   }
 
   egress {
